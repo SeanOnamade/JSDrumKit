77100235c6f989ef96be6082d2a177766c8f1e0a
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-05-07 22:57:34 -0600

    styling

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function playSound(e) {
     audio.play();
     //console.log(key);
     key.classList.add('playing');
+    // let x = getRandomColor(); // UNCOMMENT FOR FUN COLORS
+    // key.style.borderColor = x; // Apply random color
+    // key.style.boxShadow = `0 0 1rem ${x}`; // Apply random color
 }
 
 function removeTransition(e) {
@@ -17,10 +20,33 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip if it's not a transform
     // console.log(e.propertyName);
     this.classList.remove('playing');
+
     // can always check what "this" is with a console log (console.log(this);) 
     // in this case, it's equal to the key
     // ( keys.forEach(key => addEventListener('transitionend'. removeTransition));)
 
+    // this.style.borderColor = 'black'; // UNCOMMENT FOR FUN COLORS
+    // this.style.boxShadow = 'none';
+
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const keys = document.querySelectorAll('.key');
+//     const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'cyan', 'magenta', 'teal', 'lime'];
+
+//     keys.forEach(key => {
+//         const randomColor = colors[Math.floor(Math.random() * colors.length)];
+//         key.style.backgroundColor = randomColor;
+//     });
+// });
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 const keys = document.querySelectorAll('.key');
@@ -28,3 +54,4 @@ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     // each key gets transitionend event listener and when the transition ends, we remove it
 
 window.addEventListener('keydown', playSound);
+
